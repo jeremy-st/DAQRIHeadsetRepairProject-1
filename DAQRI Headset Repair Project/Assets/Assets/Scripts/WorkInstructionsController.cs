5813621637c752d9159e097eaab1a57ffd5b3993
Jeremy Stout <jeremy.stout@lacity.org>
2017-07-06 22:16:47 -0700

    Started menu

--- DAQRI Headset Repair Project/Assets/Assets/Scripts/WorkInstructionsController.cs
+++ DAQRI Headset Repair Project/Assets/Assets/Scripts/WorkInstructionsController.cs
@@ -4,56 +4,12 @@ using UnityEngine;
 
 public class WorkInstructionsController : MonoBehaviour {
 
-    public List<GameObject> steps;
-
-    private int currentIndex;
-
     void Start()
     {
-        currentIndex = 0;
-        foreach (GameObject step in steps)
-        {
-            step.gameObject.SetActive(false);
-        }
+        
+    }    
 
-        steps[currentIndex].gameObject.SetActive(true);
-    }
-
-    public void GoToNextStep()
-    {
-        if (currentIndex <= steps.Count - 2)
-        {
-            steps[currentIndex].SetActive(false);
-            currentIndex++;
-            steps[currentIndex].SetActive(true);
-            Debug.Log("Step " + currentIndex + " loaded");
-        }
-        else
-        {
-            Debug.Log("You're at the last step. No more steps to navigate to!");
-        }
-    }
-
-    public void GoToPreviousStep()
-    {
-        if (currentIndex >= 1)
-        {
-            steps[currentIndex].SetActive(false);
-            currentIndex--;
-            steps[currentIndex].SetActive(true);
-            Debug.Log("Step " + currentIndex + " loaded");
-        }
-    }
-
-    public void Restart()
-    {
-        steps[currentIndex].SetActive(false);
-        currentIndex = 0;
-        steps[currentIndex].SetActive(true);
-        Debug.Log("Step " + currentIndex + " loaded");
-    }
-
-    public void Quit()
+     public void Quit()
     {
         Debug.Log("Application Quit");
         Application.Quit();
