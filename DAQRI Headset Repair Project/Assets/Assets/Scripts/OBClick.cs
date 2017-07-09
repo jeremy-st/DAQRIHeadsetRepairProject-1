21366302717f0df4a30a9b15b66a193bbd5d59c3
Jeremy Stout <jeremy.stout@lacity.org>
2017-07-09 15:04:18 -0700

    Add Text from the objects

---
+++ DAQRI Headset Repair Project/Assets/Assets/Scripts/OBClick.cs
@@ -0,0 +1,54 @@
+﻿using DAQRI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OBClick : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject gameobj;
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {      
+        //foreach (Transform child in gameobj.transform)
+        //{
+        //    Renderer rend = child.GetComponent<Renderer>();
+        //    rend.material.shader = Shader.Find("Outlined/SilhouettedDiffuse");
+        //}              
+        EventSystem eventSystem = EventSystem.current;
+        var RIM = (ReticleInputModule)eventSystem.gameObject.GetComponent(typeof(ReticleInputModule));
+        GameObject tmp = GameObject.Find("Reticle Text");
+        var ob = (TextDisplay)gameobj.GetComponent(typeof(TextDisplay));
+       
+        if (tmp == null)
+        {            
+            GameObject TextGO = new GameObject("Reticle Text");
+            Text txt = TextGO.gameObject.AddComponent<Text>();
+            txt.text = ob.Message;
+            txt.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+            txt.fontSize = 20;
+            txt.color = Color.black;
+            txt.alignment = TextAnchor.MiddleCenter;
+            TextGO.transform.SetParent(RIM.reticle.transform);
+            TextGO.transform.localPosition = new Vector3(0f, -200f, -1.2f);
+            TextGO.transform.localScale = new Vector3(1f, 1f, 1f);
+            TextGO.transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
+            RectTransform rt = TextGO.GetComponent(typeof(RectTransform)) as RectTransform;
+            rt.sizeDelta = new Vector2(500, 30);
+        }
+        else
+        {
+            Text txt = tmp.GetComponent<Text>();
+            txt.text = ob.Message;
+        }
+    }
+}
---
+++ DAQRI Headset Repair Project/Assets/Assets/Scripts/UIScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIScript : MonoBehaviour {
+
+    public Button StartAnimation;
+    public Button ReverseAnimation;
+    public List<Animator> AnimotorList;
+    // Use this for initialization
+    void Start () {    
+        
+        StartAnimation.onClick.AddListener(StartAnimationClick);
+        ReverseAnimation.onClick.AddListener(ReverseAnimationClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Hi2");
+    }
+    void StartAnimationClick()
+    {    foreach(Animator ani in AnimotorList)
+        {
+            ani.SetBool("Start", true);
+        }       
+    }
+    void ReverseAnimationClick()
+    {
+        foreach (Animator ani in AnimotorList)
+        {
+            ani.SetBool("Start", false);
+        }
+    }
+}
